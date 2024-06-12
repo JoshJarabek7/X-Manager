f4235069c72b37bc7edf62da9ccee1bd9ec56ca7
Josh Jarabek <josh@parakeet.college>
2024-06-11 21:35:39 -0500

    Adding routes

--- backend/x_manager/db.py
+++ backend/x_manager/db.py
@@ -1,15 +1,18 @@
-import motor.motor_asyncio
 import os
+import motor.motor_asyncio
 from beanie import init_beanie
 from .models.Followers import Follower
 from .models.Following import Following
 from .models.Likes import Like
 from .models.Tweet import Tweet
 
-MONGODB_HOST = os.getenv("MONGODB_HOST", "localhost")
-MONGODB_PORT = os.getenv("MONGODB_PORT", "27017")
-MONGODB_DATABASE = os.getenv("MONGODB_DATABASE", "main")
-MONGODB_URI = f"mongodb://{MONGODB_HOST}:{MONGODB_PORT}/{MONGODB_DATABASE}"
+MONGODB_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME", "root")
+MONGODB_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "example")
+MONGODB_PORT = os.getenv("MONGO_INITDB_PORT", "27017")
+MONGODB_DATABASE = os.getenv("MONGO_INITDB_DATABASE", "main")
+MONGODB_SERVICE_NAME = os.getenv("MONGO_INITDB_SERVICE_NAME", "mongo")
+
+MONGODB_URI = f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_SERVICE_NAME}:{MONGODB_PORT}/{MONGODB_DATABASE}"
 
 
 async def start_db():
