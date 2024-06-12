9647a2e15b6567c2eb667cbf1851193bb1254473
Josh Jarabek <josh@parakeet.college>
2024-06-12 00:20:30 -0500

    Cleanup unstarted modules.

--- backend/x_manager/main.py
+++ backend/x_manager/main.py
@@ -1,8 +1,1 @@
 """Main module that will run on startup to boot the backend"""
-
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
-from .routers.rabbit.setup import start_broker, stop_broker
-
-
-
